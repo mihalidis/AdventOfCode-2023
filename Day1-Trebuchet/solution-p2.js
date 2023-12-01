816cb7e64ba99f9ef7751a0f2d6e85b8c33199f8
pinar <pinarsuvac@gmail.com>
2023-12-01 21:30:30 +0300

    day 2 solution part 2

--- Day1-Trebuchet/solution-p2.js
+++ Day1-Trebuchet/solution-p2.js
@@ -3,16 +3,28 @@ const lines = parseLines("./Day1-Trebuchet/input.txt", true);
 
 const ObjectArray = [...Object.values(lines)];
 
+const numbers = {one: 1, two: 2, three: 3, four: 4, five: 5, six: 6, seven: 7, eight: 8, nine: 9}
+const stringNums = Object.keys(numbers)
 let answer = 0
 
 ObjectArray.forEach(input => {
-  const numArray = input?.split('').reduce ((acc, item) => {
-    if (!!parseInt(item)) acc.push(parseInt(item))
+  const orderedInput = []
+  stringNums.forEach(item => {
+    let index = -1;
+    while ((index = input.indexOf(item, index + 1)) !== -1) {
+      orderedInput[index] = numbers[item];
+    }
+  })
 
-    return acc
-  }, [])
-  
-  answer += parseInt(`${numArray[0]}${numArray[numArray.length-1]}`)
+  input.split('').forEach((item, index) => {
+    if (!!parseInt(item)) {
+      orderedInput[index] = parseInt(item)
+    }
+  })
+
+  const filteredInput = orderedInput.filter(item => item !== undefined && item !== null)
+
+  answer += parseInt(`${filteredInput[0]}${filteredInput[filteredInput.length-1]}`)
 })
 
 console.log(answer)
