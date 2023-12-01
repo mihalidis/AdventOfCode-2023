15da4e6dffc36b63cd8882b476d7a6ba0c874a3c
pinar <pinarsuvac@gmail.com>
2023-12-01 10:01:47 +0300

    initial commit

---
+++ utils/index.js
@@ -0,0 +1,10 @@
+import fs from "fs";
+
+export const parseLines = (fileName, trim) => {
+    const contents = fs.readFileSync(fileName).toString().split("\n");
+    contents.pop();
+    if (trim) return contents.map((x) => x.trim());
+    return contents;
+  };
+  
+export const parseInput = (fileName) => fs.readFileSync(fileName).toString();
