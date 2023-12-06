459eaafa09357d4f10223f1a37c1d993aeb5efc4
pinar <pinarsuvac@gmail.com>
2023-12-06 16:58:07 +0300

    add day_4 part_1 Solution

--- Day4_Scratchcards/solution-p1.js
+++ Day4_Scratchcards/solution-p1.js
@@ -2,5 +2,23 @@ import { parseLines } from "../utils/index.js";
 const lines = parseLines("./Day4_Scratchcards/input.txt", true);
 
 const scratchCards = Object.values(lines);
+let answer = 0
 
-console.log(scratchCards)
+scratchCards.forEach((card, cardIndex) => {
+  const scratchCard = card.split(': ')[1].split(' | ').map(part => part.split(' ').filter(item => item !== ''))
+  let winingNumMultipled = 0
+
+  scratchCard[0].forEach(num => {
+    if (scratchCard[1].includes(num)) {
+      if (winingNumMultipled === 0) {
+        winingNumMultipled++
+      } else {
+        winingNumMultipled+=winingNumMultipled
+      }
+    }
+  })
+
+  answer += winingNumMultipled
+})
+
+console.log(answer)
